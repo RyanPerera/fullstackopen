d9a2c802f14517ad0858e10fb380bee0190dfb79
Ryan Perera <ryanperera9001@gmail.com>
2023-03-28 02:03:38 -0400

    Completed to 1.5, courseinfo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,16 +1,16 @@
+import { useState } from 'react'
+
 const Header = (props) => {
-  console.log(props.course)
   return (
     <h1>{props.course}</h1>
   )
 }
 
 const Content = (props) => {
-  console.log(props.content)
   const items = props.content
   const listItems = items.map((element, index) =>
     <Part key={index}
-      name = {element[0]} number = {element[1]}
+      name={element.name} number={element.exercises}
     />
   );
   return (
@@ -19,30 +19,43 @@ const Content = (props) => {
 }
 
 const Part = (props) => {
-  console.log(props)
-  return(
+  return (
     <p>{props.name}, {props.number}</p>
   )
 }
 
 const Total = (props) => {
-  console.log(props.total)
-  return(
-    <p>{props.total}</p>
+  const total = props.total[0].exercises + props.total[1].exercises + props.total[2].exercises
+  return (
+    <p>{total}</p>
   )
 }
 
 const App = () => {
-  //  <!-- <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> -->
-  const course = 'Half Stack application development'
-
-  const content = [[ 'Fundamentals of React', 10 ], [ 'Using props to pass data', 7 ], [ 'State of a component', 14 ]]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content content={content} />
-      <Total total = {content[0][1] + content[1][1] + content[2][1]} />
+      <Header course={course.name} />
+      <Content content={course.parts} />
+      <Total total={course.parts} />
+      <div>{counter}</div>
     </div>
   )
 }
